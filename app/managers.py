edbca026ce9d8f12120b8052c94402ca210e3b56
acidcor <deadfoxua1337@gmail.com>
2025-10-30 23:07:48 +0100

    done task

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,47 @@
 import sqlite3
+from typing import List
 
-from app.models import Actor
+from app.models import Actors
 
 
-# add manager here
+class ActorManager:
+    def __init__(self, db_name: str, table_name: str) -> None:
+        self._connection = sqlite3.connect(db_name)
+        self.table_name = table_name
+
+    # Create - C
+    def create(
+        self,
+        first_name: str,
+        last_name: str,
+    ) -> None:
+        self._connection.execute(
+            f"INSERT INTO {self.table_name} (first_name, last_name)"
+            f" VALUES (?, ?)",
+            (first_name, last_name),
+        )
+        self._connection.commit()
+
+    # RETRIEVE - R
+    def all(self) -> List[Actors]:
+        literary_format_cursor = self._connection.execute(
+            f"SELECT * FROM {self.table_name}"
+        )
+        return [Actors(*row) for row in literary_format_cursor]
+
+    # UPDATE - U
+    def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
+        self._connection.execute(
+            f"UPDATE {self.table_name} "
+            "SET first_name = ? , last_name = ? "
+            "WHERE id = ? ",
+            (new_first_name, new_last_name, pk),
+        )
+        self._connection.commit()
+
+    # DELETE - D
+    def delete(self, pk: int) -> None:
+        self._connection.execute(
+            f"DELETE FROM {self.table_name} " "WHERE id = ? ", (pk,)
+        )
+        self._connection.commit()
--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass
 
 
-# add dataclass here
+@dataclass
+class Actors:
+    id: int
+    first_name: str
+    last_name: str
